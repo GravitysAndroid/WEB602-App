e7dcb3358f2a41e869a68d17e8bbb6ac39045bd6
Hayden <haydenwill95@gmail.com>
2020-11-06 13:07:33 +1300

    Added the parts that are needed for comments and login

--- api/Dataservice/dataservice.js
+++ api/Dataservice/dataservice.js
@@ -1,12 +1,69 @@
 let mysql = require('mysql');
 
-function GetUser(aResFunction){
-    let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'hayden',
-        password: '8576',
-        database: 'hayden'
-    });
+function StoreComment(aResFunction, pUser, pComment){
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hayden',
+    password: '8576',
+    database: 'hayden'
+});
+
+  connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
+
+  let sql = `Call StoreComment(?,?)`;
+  console.log([pUser,pComment]);
+  connection.query(sql, [pUser,pComment], (error, results, fields) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    console.log(results[0]);
+    aResFunction(JSON.stringify(results[0]));
+  });
+  connection.end();
+}
+
+function CheckPassword(aResFunction, pUser, pPassword){
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hayden',
+    password: '8576',
+    database: 'hayden'
+});
+
+  connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
+
+  let sql = `Call CheckPassword(?,?)`;
+  console.log([pUser,pPassword]);
+  connection.query(sql, [pUser,pPassword], (error, results, fields) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    console.log(results[0]);
+    aResFunction(JSON.stringify(results[0]));
+  });
+  connection.end();
+}
+
+
+function GetUsers(aResFunction){
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hayden',
+    password: '8576',
+    database: 'hayden'
+});
 
     connection.connect(function(err) {
         if (err) {
@@ -28,4 +85,6 @@ function GetUser(aResFunction){
       connection.end();
 }
 
-module.exports = GetUser; 
+exports.GetUsers = GetUsers;
+exports.CheckPassword = CheckPassword; 
+exports.StoreComment = StoreComment; 
